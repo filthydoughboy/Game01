7bbb5618765adca2d1f0c1874327d8d0b9b76c30
filthydoughboy <jp882@cornell.edu>
2016-06-03 07:22:12 -0400

    not sure if this is the right repo

---
+++ desktop/src/com/mygdx/game01/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.game01.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.game01.Game01;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game01(), config);
+	}
+}
